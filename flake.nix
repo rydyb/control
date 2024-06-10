128a8a6de2a20c506430321c7a9bcc59cac38b81
Bodo Kaiser <bodo.kaiser@physik.uni-muenchen.de>
2024-06-10 17:48:43 +0200

    rewritting flake.nix from scratch

--- flake.nix
+++ flake.nix
@@ -1,23 +1,18 @@
 {
-  inputs.extrapkg.url = "git+https://git.m-labs.hk/M-Labs/artiq-extrapkg.git";
-  outputs = { self, extrapkg }:
+  inputs = {
+    artiq.url = "github:m-labs/artiq/release-8";
+  };
+
+  outputs = { self, artiq }:
     let
-      pkgs = extrapkg.pkgs;
-      artiq = extrapkg.packages.x86_64-linux;
+      pkgs = artiq.inputs.nixpkgs.x86_64-linux;
     in {
-      packages.x86_64-linux.default = pkgs.buildEnv {
-        name = "artiq-env";
-        paths = [
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
           (pkgs.python3.withPackages(ps: [
-            artiq.artiq
-            artiq.flake8-artiq
+            artiq.packages.x86_64-linux.artiq
           ]))
-          pkgs.vscodium
         ];
       };
     };
-  nixConfig = {  # work around https://github.com/NixOS/nix/issues/6771
-    extra-trusted-public-keys = "nixbld.m-labs.hk-1:5aSRVA5b320xbNvu30tqxVPXpld73bhtOeH6uAjRyHc=";
-    extra-substituters = "https://nixbld.m-labs.hk";
-  };
 }
