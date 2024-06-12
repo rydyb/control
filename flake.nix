2fe5a71591cc073443a47cfb66e60a1740dde2ff
Bodo Kaiser <bodo.kaiser@physik.uni-muenchen.de>
2024-06-12 14:52:53 +0200

    added setup.py, control/__init__.py, updated flake.nix to add control py package

--- flake.nix
+++ flake.nix
@@ -1,18 +1,30 @@
 {
   inputs = {
+    nixpkgs.follows = "artiq/nixpkgs";
+
     artiq.url = "github:m-labs/artiq/release-8";
   };
 
-  outputs = { self, artiq }:
+  outputs = { self, nixpkgs, artiq }:
     let
-      pkgs = artiq.inputs.nixpkgs.x86_64-linux;
-    in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = [
-          (pkgs.python3.withPackages(ps: [
-            artiq.packages.x86_64-linux.artiq
-          ]))
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      control = pkgs.python3Packages.buildPythonPackage {
+        pname = "rydyb-control";
+        version = "0.0.0";
+        src = self;
+        nativeBuildInputs = [ pkgs.qt5.wrapQtAppsHook ];
+        propagatedBuildInputs = [
+          artiq.packages.x86_64-linux.artiq
         ];
       };
+    in rec {
+      packages.x86_64-linux = {
+        inherit control;
+      };
+
+      packages.x86_64-linux.default = pkgs.python3.withPackages(ps: [
+        packages.x86_64-linux.control
+      ]);
     };
 }
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rydyb-control",
+    version="0.0.0",
+    url="https://github.com/rydyb/control",
+    packages=find_packages(),
+)
